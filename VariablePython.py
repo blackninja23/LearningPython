e929f3f0a6f333098ca026a59f7fa5ab235afdf2
blackninja23 <78839207+blackninja23@users.noreply.github.com>
2021-02-10 15:45:44 +0300

    Add files via upload

---
+++ VariablePython.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+
+x=5
+y='Hello, World'
+z="Hello, World"
+
+a=int(4)
+b=str("Hello World")
+c=float(6)
+print("Value of X")
+print(x)
+print("Value of y")
+print(y)
+print("Value of z")
+print(z)
+print("\n\nData type of a")
+print(type(a))
+print("\nData type of b")
+print(type(b))
+print("\nData type of c")
+print(type(c))
+
+
+
